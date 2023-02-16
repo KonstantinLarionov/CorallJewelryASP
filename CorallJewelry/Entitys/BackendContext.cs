ba6f41bbeda3e368d1559039776d6963494710f4
Иоан <polskikh.ioan@gmail.com>
2023-02-16 12:12:17 +0500

    appsettings change

--- CorallJewelry/Entitys/BackendContext.cs
+++ CorallJewelry/Entitys/BackendContext.cs
@@ -25,10 +25,11 @@ namespace CorallJewelry.Entitys
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseMySql("Server=localhost;Database=u1810457_coralljewelry;User=u1810457_corall;Password=sOq2e&032;Zero Datetime=True");
+            //optionsBuilder.UseMySql("DefaultConnection");
+            //optionsBuilder.UseMySql("Server=localhost;Database=u1810457_coralljewelry;User=u1810457_corall;Password=sOq2e&032;ZeroDatetime=True");
             //optionsBuilder.UseMySql("Server=localhost;Database=u1810457_default;User=u1810_457default;Password=ZT5U2b2pSU4Zalnc;ZeroDatetime=True");
             //optionsBuilder.UseMySql("Server=localhost;Database=u0959678_coralljewelry;User=root;Password=root;Convert Zero Datetime=True");
-            //optionsBuilder.UseMySql("Server=localhost;Database=u1810457_coralljewelry;User=root;Password=root;ConvertZeroDatetime=True");
+            optionsBuilder.UseMySql("Server=localhost;Database=u1810457_coralljewelry;User=root;Password=root;ConvertZeroDatetime=True");
             base.OnConfiguring(optionsBuilder);
         }
     }
